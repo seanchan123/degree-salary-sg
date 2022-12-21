01e6c1a7bd75c193357c9562174a62db06f364d2
Sean Chan <seanchan@outlook.sg>
2022-12-21 17:49:56 +0800

    Format document

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export const getStaticProps = async () => {
 
   // Fetch latest data
   var latestData: dataStore = await (await fetch('https://data.gov.sg/api/action/datastore_search?resource_id=3a60220a-80ae-4a63-afde-413f05328914&limit=1&sort=year desc')).json();
-  
+
   // Fetch all data based on previous query
   const totalRecords: number = oldestData.result.total;
   var allData: dataStore = await (await fetch('https://data.gov.sg/api/action/datastore_search?resource_id=3a60220a-80ae-4a63-afde-413f05328914&limit=' + totalRecords + '&sort=year desc')).json();
@@ -48,7 +48,6 @@ export const getStaticProps = async () => {
   for (let currentYear: number = parseInt(oldestData.result.records[0].year); currentYear <= parseInt(latestData.result.records[0].year); currentYear++) {
     years.push(currentYear);
   }
-  
 
   return {
     props: { dataSet: allData, availableYears: years }
