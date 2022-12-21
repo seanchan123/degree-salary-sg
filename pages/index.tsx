f96cf77f21287e99b5045d92e5ea6ed4bcfabf0c
Sean Chan <seanchan@outlook.sg>
2022-12-21 22:07:32 +0800

    Removed unused packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react';
-import styles from '../styles/Home.module.css'
 
 type dataStore = {
   help: string,
