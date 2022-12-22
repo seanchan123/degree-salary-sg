7511a371420e097e257e05bcf59e9c3ffb159f03
Sean Chan <seanchan@outlook.sg>
2022-12-22 23:05:34 +0800

    Added example Navigation Bar & Body

--- pages/index.tsx
+++ pages/index.tsx
@@ -154,6 +154,60 @@ const Test = ({ records, fields, years, universities, schools, degrees }: { reco
       })
   };
 
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Docs
+        </a>
+      </Typography>
+    </ul>
+  );
+
   return (
     <>
       <Head>
@@ -163,10 +217,79 @@ const Test = ({ records, fields, years, universities, schools, degrees }: { reco
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+
+        <nav className="block rounded-xl shadow-md backdrop-saturate-200 backdrop-blur-2xl bg-opacity-80 border border-white/80 bg-white text-white mx-0 w-screen py-2 px-4 lg:px-8 lg:py-4">
+          <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+            <Typography
+              as="a"
+              href="#"
+              variant="small"
+              className="mr-4 cursor-pointer py-1.5 font-normal"
+            >
+              <span>Material Tailwind</span>
+            </Typography>
+            <div className="hidden lg:block">{navList}</div>
+            <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+              <span>Buy Now</span>
+            </Button>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+          <MobileNav open={openNav}>
+            {navList}
+            <Button variant="gradient" size="sm" fullWidth className="mb-2">
+              <span>Buy Now</span>
+            </Button>
+          </MobileNav>
+        </nav>
+
+        <div className="text-center bg-gray-50 text-gray-800 py-20 px-6">
+          <h1 className="text-5xl font-bold mt-0 mb-6">Heading</h1>
+          <h3 className="text-3xl font-bold mb-8">Subeading</h3>
+          <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
+        </div>
+
         <div>
+
           <Bar data={data} />
           {/* <Scatter data={data} /> */}
         </div>
+
       </main>
     </>
   )
