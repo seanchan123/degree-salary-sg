e985af1e62e3ffbf958e912087c48543d380b687
Sean Chan <seanchan@outlook.sg>
2022-12-22 19:44:28 +0800

    Changed structure of `colors` to include more color schemes

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,13 +35,11 @@ type dataStore = {
   year: number[]
 }
 
-const colors: string[] = [
-  "rgba(230, 25, 27, 1)",
-  "rgba(245, 130, 49, 1)",
-  "rgba(255, 225, 25, 1)",
-  "rgba(60, 180, 75, 1)",
-  "rgba(66, 99, 216, 1)",
-  "rgba(240, 50, 230, 1)"
+const colors: string[][] = [
+  // Color Scheme #0 (Default - Shade of Primary Color)
+  ["rgba(25, 69, 105, 1)", "rgba(95, 132, 162, 1)", "rgba(145, 174, 196, 1)", "rgba(183, 208, 225, 1)", "rgba(202, 222, 237, 1)", "rgba(219, 236, 244, 1)"],
+  // Color Scheme #1 (Distinct - Rainbow)
+  ["rgba(230, 25, 27, 1)", "rgba(245, 130, 49, 1)", "rgba(255, 225, 25, 1)", "rgba(60, 180, 75, 1)", "rgba(66, 99, 216, 1)", "rgba(240, 50, 230, 1)"]
 ]
 
 export const getStaticProps = async () => {
@@ -143,7 +141,7 @@ const Test = ({ records, fields, years, universities, schools, degrees }: { reco
           }).map(record => {
             return { x: record[fields[0] as keyof dataRecord], y: record[fields[5] as keyof dataRecord] }
           }),
-          backgroundColor: colors[universities.indexOf(university)]
+          backgroundColor: colors[0][universities.indexOf(university)]
         }
       })
   };
