734e0902b624d771a7b7ea704eaa7815a87309c3
Sean Chan <seanchan@outlook.sg>
2022-12-22 23:07:31 +0800

    Added <header> tag to enclose navigation bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -217,66 +217,68 @@ const Test = ({ records, fields, years, universities, schools, degrees }: { reco
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
-        <nav className="block rounded-xl shadow-md backdrop-saturate-200 backdrop-blur-2xl bg-opacity-80 border border-white/80 bg-white text-white mx-0 w-screen py-2 px-4 lg:px-8 lg:py-4">
-          <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-            <Typography
-              as="a"
-              href="#"
-              variant="small"
-              className="mr-4 cursor-pointer py-1.5 font-normal"
-            >
-              <span>Material Tailwind</span>
-            </Typography>
-            <div className="hidden lg:block">{navList}</div>
-            <Button variant="gradient" size="sm" className="hidden lg:inline-block">
-              <span>Buy Now</span>
-            </Button>
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
-          </div>
-          <MobileNav open={openNav}>
-            {navList}
-            <Button variant="gradient" size="sm" fullWidth className="mb-2">
-              <span>Buy Now</span>
-            </Button>
-          </MobileNav>
-        </nav>
+        
+        <header>
+          <nav className="block rounded-xl shadow-md backdrop-saturate-200 backdrop-blur-2xl bg-opacity-80 border border-white/80 bg-white text-white mx-0 w-screen py-2 px-4 lg:px-8 lg:py-4">
+            <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+              <Typography
+                as="a"
+                href="#"
+                variant="small"
+                className="mr-4 cursor-pointer py-1.5 font-normal"
+              >
+                <span>Material Tailwind</span>
+              </Typography>
+              <div className="hidden lg:block">{navList}</div>
+              <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+                <span>Buy Now</span>
+              </Button>
+              <IconButton
+                variant="text"
+                className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                ripple={false}
+                onClick={() => setOpenNav(!openNav)}
+              >
+                {openNav ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    className="h-6 w-6"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </IconButton>
+            </div>
+            <MobileNav open={openNav}>
+              {navList}
+              <Button variant="gradient" size="sm" fullWidth className="mb-2">
+                <span>Buy Now</span>
+              </Button>
+            </MobileNav>
+          </nav>
+        </header>
 
         <div className="text-center bg-gray-50 text-gray-800 py-20 px-6">
           <h1 className="text-5xl font-bold mt-0 mb-6">Heading</h1>
@@ -285,7 +287,6 @@ const Test = ({ records, fields, years, universities, schools, degrees }: { reco
         </div>
 
         <div>
-
           <Bar data={data} />
           {/* <Scatter data={data} /> */}
         </div>
