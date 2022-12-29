c665127bbd5e173fb020fce9f9ab773e04dce0fe
Sean Chan <seanchan@outlook.sg>
2022-12-30 03:26:50 +0800

    Formatted document

--- pages/index.tsx
+++ pages/index.tsx
@@ -602,7 +602,9 @@ const Index = ({
                       id='default-search'
                       className='block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
                       placeholder='Search Schools, Degrees...'
-                      onChange={(event) => { updateSearch(event.target.value); }}
+                      onChange={(event) => {
+                        updateSearch(event.target.value);
+                      }}
                     />
                   </div>
                 </form>
@@ -656,7 +658,9 @@ const Index = ({
                       }`}
                       onClick={() => updateUniversityFilter(index)}
                     >
-                      <span>{university.replace(/[a-z]/g, '').replace(/ /g, '')}</span>
+                      <span>
+                        {university.replace(/[a-z]/g, '').replace(/ /g, '')}
+                      </span>
                     </Button>
                   );
                 })}
