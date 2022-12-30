cb90e5c1fd53f06e8058e41b067c95188cc56c8c
Humphrey <vanillaqicecream@gmail.com>
2022-12-31 04:46:15 +0800

    refactor: add src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require('@material-tailwind/react/utils/withMT');
 
 // tailwind.config.js
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 
 module.exports = withMT({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#194569',
-        slate: colors.slate
+        primary: '#194569',
+        slate: colors.slate,
       },
     },
   },
